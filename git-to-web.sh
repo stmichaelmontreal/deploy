67db1e70d87f5aec98fba4f3519e2ab8a9ddea35
Sergii Lutchyn <stmichaelmontreal@gmail.com>
2019-01-19 00:48:05 -0500

    Add file to deploy

--- git-to-web.sh
+++ git-to-web.sh
@@ -1,11 +1,6 @@
-sudo git clone https://github.com/stmichaelmontreal/zip.git
+git clone https://github.com/stmichaelmontreal/deploy.git
 sudo systemctl stop nginx
-sudo unzip zip/sm.zip
+sudo rm -rf /var/www/html/*
+sudo unzip deploy/deploy.zip -d /var/www/html
 sudo systemctl start nginx
-
-cd /var/www/html/
-sudo cp zip/sm.zip *
-sudo rm -rf zip
-sudo cp sm.zip .
-sudo rm -rf sm.zip
-
+sudo rm -rf deploy
