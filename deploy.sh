fb30ee1a245d53234014f96b89cddfcf1e6310cd
Sergii Lutchyn <stmichaelmontreal@gmail.com>
2019-01-19 00:12:22 -0500

    Add file to deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,10 @@
 cd ..
 npm run prod
 cd deploy/sm
-zip -rm9 sm *
-mv sm.zip ../sm.zip
+zip -rm9 deploy *
+mv deploy.zip ../deploy.zip
 cd ..
 rm -d sm
+git add .
+git commit -m "Add file to deploy"
+git push origin master
